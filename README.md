0f49b09f4ecedc0114eca0996b0227ab0a982bab
MayNiklas <info@niklas-steffen.de>
2024-03-11 10:47:15 +0100

    implement absolute paths

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # p1s-avi-converter
+
+My BambuLab P1S 3D printer records timelapse vidoes in AVI format. This is a simple CLI tool, to convert them into MP4 format.
--- avi_converter/__init__.py
+++ avi_converter/__init__.py
@@ -1,25 +1,33 @@
-import ffmpeg
 import argparse
 import pathlib
 
+import ffmpeg
+
+
+def get_absolute_path(path):
+    """
+    Converts a relative or absolute path to an absolute path.
+    """
+
+    if pathlib.Path(path).is_absolute():
+        return pathlib.Path(path)
+    else:
+        return pathlib.Path(path).resolve()
+
 
 def cli():
-
     parser = argparse.ArgumentParser(description='Convert avi to mp4')
     parser.add_argument('input', type=open)
     parser.parse_args()
 
-    # TODO: absolute paths should also work
-    input_path = pathlib.Path(parser.parse_args().input.name).resolve()
+    input_path = get_absolute_path(parser.parse_args().input.name)
     output_path = input_path.with_suffix('.mp4')
-
     print(f'Converting {input_path} to {output_path}')
 
-    # TODO: crf should be a parameter
     (
         ffmpeg
-        .input(input_path.name)
-        .output(output_path.name, **{'crf': 28})
+        .input(input_path)
+        .output(str(output_path), **{'crf': 28})
         .run()
     )
 
