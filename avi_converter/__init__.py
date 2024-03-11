6470b73e180e90e5273a54e776dba52670a2e05c
MayNiklas <info@niklas-steffen.de>
2024-03-11 10:28:31 +0100

    add todo

--- avi_converter/__init__.py
+++ avi_converter/__init__.py
@@ -9,11 +9,13 @@ def cli():
     parser.add_argument('input', type=open)
     parser.parse_args()
 
+    # TODO: absolute paths should also work
     input_path = pathlib.Path(parser.parse_args().input.name).resolve()
     output_path = input_path.with_suffix('.mp4')
 
     print(f'Converting {input_path} to {output_path}')
 
+    # TODO: crf should be a parameter
     (
         ffmpeg
         .input(input_path.name)
